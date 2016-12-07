c3bf2b1713ec57b3ac1ef60bcdea13f604db2ad2
StevenGuo5033 <stevenguo5033@gmail.com>
2016-12-07 11:02:45 -0800

    stm

---
+++ STM/STM 1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 10:09:59 2016
+
+@author: StevenG
+"""
+
+
+def 
+    
+    
+    
+    
+for i in range (0,nx):
+
+    if (i+1)%2=1:  
+        for j in range (0,ny):  
+            SetVx(i)
+            SetVy(j)   
+    else:
+        for j in range (0,ny):
+            SetVx(i)
+            SetVy(ny-j)
+            
